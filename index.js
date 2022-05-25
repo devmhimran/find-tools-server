0452c3d2fe5d6e86f21c8489cb15aa12ca2abf14
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-05-26 05:21:26 +0600

    Added User Email Send To Database

--- index.js
+++ index.js
@@ -1,17 +1,63 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
+require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
-// f49a9369d3cd4355c5c89d61fa2fbc71fa5b45e2a0520d11bd56e2cdf417e27b8a4a51f0feaa31e999bd7dff23d8b3c001b46042d10d34849e2099dafd991bad
+app.use(cors());
+app.use(express.json());
 
+const uri = "mongodb+srv://:<password>@cluster0.emnmy.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const verfiyJWT = async (req, res, next) => {
+
+}
+
+
+
+async function run() {
+  try {
+    await client.connect();
+    const userCollection = client.db('Find-Tools').collection('users');
+    // const productCollection = client.db('Find-tools').collection('users');
+
+    // app.get('/products', (req, res)=>{
+
+    // })
+
+    app.put('/user/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
+      // const updateDoc = {
+      //   $set: user,
+      // };
+      // const result = await userCollection.updateOne(filter, updateDoc, options);
+      // res.send(result);
+      console.log(email);
+      
+    })
+
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send('Find Tools Works Successfully')
-  })
-  
-  app.listen(port, () => {
-    console.log(`Find Tools Works Successfully on port ${port}`)
-  })
+  res.send('Find Tools Works Successfully')
+})
+
+app.listen(port, () => {
+  console.log(`Find Tools Works Successfully on port ${port}`)
+})
