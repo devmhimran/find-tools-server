6028ed81a6ffbd06b7012aa6391e1019c5491ddc
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-05-26 17:02:12 +0600

    Added Make Admin Option

--- index.js
+++ index.js
@@ -26,7 +26,6 @@ function verifyJWT(req, res, next) {
     req.decoded = decoded;
     next();
   });
-  console.log(token);
 }
 
 
@@ -55,6 +54,16 @@ async function run() {
       res.send(users);
     });
 
+    app.put('/user/admin/:email', async (req, res) => {
+      const email = req.params.email;
+      const filter = { email: email };
+      const updateDoc = {
+        $set: {role: 'admin'},
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+
+    })
     app.put('/user/:email', async (req, res) => {
       const email = req.params.email;
       const user = req.body;
