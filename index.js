56c6451600b1d06633f5fac30850e771b39f874b
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-05-26 16:45:29 +0600

    Added JWT & Product Post System

--- index.js
+++ index.js
@@ -13,9 +13,21 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.emnmy.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-// const verfiyJWT = async (req, res, next) => {
-
-// }
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: 'UnAuthorized access' });
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: 'Forbidden access' })
+    }
+    req.decoded = decoded;
+    next();
+  });
+  console.log(token);
+}
 
 
 
@@ -23,11 +35,25 @@ async function run() {
   try {
     await client.connect();
     const userCollection = client.db('Find-Tools').collection('users');
-    // const productCollection = client.db('Find-tools').collection('users');
+    const productCollection = client.db('Find-Tools').collection('products');
 
     // app.get('/products', (req, res)=>{
 
     // })
+
+    app.get('/products', async (req, res)=>{
+      const query = {};
+      const cursor = productCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+
+    app.get('/users', async (req, res)=>{
+      const query = {};
+      const cursor = userCollection.find(query);
+      const users = await cursor.toArray();
+      res.send(users);
+    });
 
     app.put('/user/:email', async (req, res) => {
       const email = req.params.email;
@@ -38,9 +64,15 @@ async function run() {
         $set: user,
       };
       const result = await userCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN, { expiresIn: '24h' });
+      res.send({result, token});
+
+    })
+
+    app.post('/products',verifyJWT, (req, res)=>{
+      const addProduct =  req.body;
+      const result = productCollection.insertOne(addProduct);
       res.send(result);
-      console.log(email);
-      
     })
 
   } finally {
