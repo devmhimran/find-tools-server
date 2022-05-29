1b03996111e7dadcb6b60575f175f217bf96edd2
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-05-29 19:26:37 +0600

    Added Stripe Payment option

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 app.use(cors());
 app.use(express.json());
@@ -108,6 +108,17 @@ async function run() {
       const query = {_id: ObjectId(id)};
       const order = await ordersCollection.findOne(query);
       res.send(order);
+    })
+    app.post('/create-payment-intent',verifyJWT, async(req, res) =>{
+      const order = req.body;
+      const price = order.price;
+      const amount = price *100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount : amount,
+        currency: 'usd',
+        payment_method_types:['card']
+      })
+      res.send({clientSecret: paymentIntent.client_secret});
     })
     // app.put('/orders/:id', verifyJWT,async (req, res)=>{
     //   const ordersId = req.params.id;
